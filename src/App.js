b144eda80ebac1b6c9c5ae99981c7df61ecb912d
Zevi Liao <jointjm@gmail.com>
2021-03-18 07:54:13 +0800

    handler

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@
 import { useState } from 'react';
 import './App.css';
 
-const shadow = {
-  boxShadow: '0 0 10px 10px #eaeaea',
-  padding: 20, // 省略 px，樣式會自動帶入單位變成 '20px'
-};
+// const shadow = {
+//   boxShadow: '0 0 10px 10px #eaeaea',
+//   padding: 20, // 省略 px，樣式會自動帶入單位變成 '20px'
+// };
+
+
+
 
 function App() {
-  const [count, setCount] = useState(256)
-
+  const [count, setCount] = useState(2)
+  const handleIncrement = () => setCount(count + 1)
+  const handleDecrement = () => setCount(count - 1)
   return (
-    <div className="container" style={shadow}>
+    <div className="container" /*style={shadow}*/> 
       {console.log('render')}
       <div className="chevron chevron-up"
-        onClick={() => {
-          setCount(count + 1)
+        style={{
+          visibility: count >= 3 && 'hidden',
         }}
+        onClick={handleIncrement}
       />
       <div className="number">{count}</div>
       <div className="chevron chevron-down"
-        onClick={() => {
-          setCount(count - 1)
+        style={{
+          visibility: count <= 0 && 'hidden',
         }}
+        onClick={handleDecrement}
       />
     </div>
   );
